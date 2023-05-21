5cc94a853feab389f81ad41d288333d42da9a9ed
apollozr <apollo.zero@outlook.com>
2023-05-21 11:32:01 -0300

    Lista de compras

---
+++ lista_de_compras.py
@@ -0,0 +1,41 @@
+import os
+
+lista = []
+while True:
+    print("Selecione uma opcao: ")
+    opcao = input('[i]nserir [a]pagar [l]istar: ')
+
+    if opcao == 'i':
+        os.system('cls')
+        valor = input('Valor: ')
+        lista.append(valor)
+    elif opcao == 'a':
+        try:
+          indice = (int(input('Escolha o indice para apagar: ')))
+          del lista[indice]
+        except:
+            print('Nao foi possivel apagar este indice!')
+    elif opcao == 'l':
+        os.system('cls')
+
+        if len(lista) == 0:
+          print('Nada para listar!')
+    
+        for i, valor in enumerate(lista):
+          print(i, valor)
+
+
+
+            
+
+
+
+      
+    
+
+
+
+
+
+
+         
